6180e23de779d73171a3f85dd70556ac8f4489be
Julio Hernandez <eduardo.hernandez@congresoqroo.gob.mx>
2020-10-28 16:54:58 -0500

    A: Se agregaron más modelos a la BD

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,29 @@
+# Admin tools
 from django.contrib import admin
 
-# Register your models here.
+# Models 
+from .models import (Professors, Students, Courses,
+                    Lessons, Questions, Answers)
+
+
+class CoursesAdmin(admin.ModelAdmin):
+    list_display = ('title', 'professor', 'created_at')
+
+
+class QuestionsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lesson', 'created_at')
+    
+
+class AnswersAdmin(admin.ModelAdmin):
+    list_display = ('title', 'question', 'question_lesson' , 'is_correct', 'created_at')
+
+    def question_lesson(self, obj):
+        return obj.question.lesson.title
+
+
+admin.site.register(Professors)
+admin.site.register(Students)
+admin.site.register(Courses, CoursesAdmin)
+admin.site.register(Lessons)
+admin.site.register(Questions, QuestionsAdmin)
+admin.site.register(Answers, AnswersAdmin)
--- api/models.py
+++ api/models.py
@@ -1,9 +1,12 @@
+# Models
 from django.db import models
 from django.contrib.auth.models import User
 
+# Choices
+from .choices import TYPE_QUESTIONS_CHOICES
 
 class Professors(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     
     class Meta:
         verbose_name = 'Profesor'
@@ -16,7 +19,7 @@ class Professors(models.Model):
 class Courses(models.Model):
     title = models.CharField(max_length=100, verbose_name='Título') 
     description = models.TextField(verbose_name='Descripción')
-    correlative_id = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name='Correlativo')
+    correlative_id = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE, verbose_name='Correlativo')
     professor = models.ForeignKey('Professors', on_delete=models.CASCADE, verbose_name='Profesor')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
 
@@ -24,9 +27,12 @@ class Courses(models.Model):
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
 
+    def __str__(self):
+        return self.title
+
 
 class Students(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     courses = models.ManyToManyField('Courses', through="RelationStudentsCourses", related_name='students')
 
     class Meta:
@@ -36,8 +42,62 @@ class Students(models.Model):
     def __str__(self):
         return self.user.get_full_name()
 
+
 class RelationStudentsCourses(models.Model):
     course = models.ForeignKey('Courses', on_delete=models.CASCADE, verbose_name='Curso')
     student = models.ForeignKey('Students', on_delete=models.CASCADE, verbose_name='Estudiante')
     approved = models.BooleanField(default=False, verbose_name='Aprobado')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+
+
+class Lessons(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Título') 
+    correlative = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE, verbose_name='Correlativo')
+    is_mandatory = models.BooleanField(default=False, verbose_name='Obligatorio')
+    approval_score = models.FloatField(null=True, blank=True, verbose_name='Puntaje de aprobación')
+    courses = models.ForeignKey('Courses', on_delete=models.CASCADE, related_name='lessons')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+
+    class Meta:
+        verbose_name = 'Lección'
+        verbose_name_plural = 'Lecciones'
+
+    def __str__(self):
+        return self.title
+
+
+class Questions(models.Model): 
+    title = models.CharField(max_length=100, verbose_name='Pregunta') 
+    score = models.FloatField(default=1.0, verbose_name='Puntaje')
+    lesson = models.ForeignKey('Lessons', on_delete=models.CASCADE, verbose_name='Lección')
+    question_type = models.IntegerField(choices=TYPE_QUESTIONS_CHOICES, verbose_name='Tipo de pregunta')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+
+    class Meta:
+        verbose_name = 'Pregunta'
+        verbose_name_plural = 'Preguntas'
+
+    def __str__(self):
+        return self.title
+
+
+class RelationStudentsLessons(models.Model):
+    student = models.ForeignKey('Students', on_delete=models.CASCADE, verbose_name='Estudiante')
+    lesson = models.ForeignKey('Lessons', on_delete=models.CASCADE, verbose_name='Lección')
+    assigned_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de asignación')
+    approved = models.BooleanField(default=False, verbose_name='Aprobado')
+    student_score = models.FloatField(default=0.0, verbose_name='Puntaje obtenido')
+
+
+class Answers(models.Model):
+    question = models.ForeignKey('Questions', on_delete=models.CASCADE, verbose_name='Pregunta')
+    title = models.CharField(max_length=100, verbose_name='Respuesta')
+    is_correct = models.BooleanField(default=False, verbose_name='¿Esta respuesta es correcta?')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    
+    class Meta:
+        verbose_name = 'Respuesta'
+        verbose_name_plural = 'Respuestas'
+
+    def __str__(self):
+        return self.title
